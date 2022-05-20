89466d198b1222edb5cdb5e73dce215347205a17
helsaalika <helsa.alika.tif421@polban.ac.id>
2022-05-20 15:57:44 +0700

    template web v2.0

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include('header.php'); ?>
+    <main>
+
+        <!-- Slider Area Start-->
+        <div class="slider-area ">
+            <div class="slider-active">
+                <div class="single-slider slider-height d-flex align-items-center" data-background="assets/img/hero/h1_hero.png">
+                    <div class="container">
+                        <div class="row d-flex align-items-center">
+                            <div class="col-lg-7 col-md-9 ">
+                                <div class="hero__caption"><br><br>
+                                    <h1 data-animation="fadeInLeft" data-delay=".4s">Welcome ! <br> We Are B5</h2>
+                                    <p data-animation="fadeInLeft" data-delay=".6s">A B5 group under the care of manager Mr. Akhmad Bakhrun consisting of 6 students, consisting of 4 women and 2 men. We are currently taking a project course with a core discussion of practical technologies that we will need skills when entering the world of work. Desktop Software Development course as a form of training to train the collaborative ability between students</p>
+                                    <!-- Hero-btn -->
+                                    <div class="hero__btn" data-animation="fadeInLeft" data-delay=".8s">
+                                        <a href="about.php" class="btn hero-btn">Let's see our profile ! </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-5">
+                                <div class="hero__img d-none d-lg-block" data-animation="fadeInRight" data-delay="1s">
+                                    <img src="assets/img/hero/hero_right.png" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+<?php include('footer.php'); ?>        
